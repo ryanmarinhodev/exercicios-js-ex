50b9e3b7d22c3734e887855ff831dfbf9ac3faa4
ryanmarinhodev <ryanmarinhodev@gmail.com>
2025-02-20 16:22:33 -0300

    features com newton

---
+++ src/ryan/calcul-triangulo/index.js
@@ -0,0 +1,12 @@
+const comprimento = 20;
+const largura = 10;
+// const perimetro = 15;
+
+function calcularArea(largura, comprimento, perimetro) {
+  return comprimento * largura;
+}
+console.log(calcularArea(largura, comprimento));
+
+function calcularPerimetro(largura, comprimento, perimetro) {
+  return comprimento * largura;
+}
---
+++ src/ryan/cancaten-string/index.js
@@ -0,0 +1,5 @@
+const nome = "Ryan";
+const cidade = "Guarabira";
+const idade = 21;
+
+console.log(`O nome do usuário é ${nome}, é de ${cidade}, e tem ${idade} anos`);
